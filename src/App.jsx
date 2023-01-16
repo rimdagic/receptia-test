2ab4b13553b13e5620c5702de9bc52f15cea6a87
SimonLeBuono123 <simon.villoresi@gmail.com>
2023-01-16 13:32:57 +0100

    Added a new file with a paragraf Element

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ export default function (){
             <Headline/>
             <Navigation/>
             <Start/>
+            <HelloHi />
+            
         </main>
         </>
 }
---
+++ src/helloHi.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function helloHi() {
+  return (
+    <p>Hello There</p>
+  
+  )
+}
